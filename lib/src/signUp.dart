5e00a0af162004c322f38f96a7ead4ecbb5fa1ca
umang <officee7788@gmail.com>
2024-03-08 16:14:12 +0530

    signup.....

--- lib/src/signUp.dart
+++ lib/src/signUp.dart
@@ -1,11 +1,17 @@
+import 'dart:developer';
+import 'dart:io';
+
 import 'package:city_serve/src/location/googleLocation.dart';
 import 'package:city_serve/navigationBar.dart';
 import 'package:city_serve/utils/dimension.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bounce/flutter_bounce.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:image_picker/image_picker.dart';
 
 import '../login/util.dart';
 import '../utils/colors.dart';
@@ -18,41 +24,125 @@ class SignUp extends StatefulWidget {
 }
 
 class _SignUpState extends State<SignUp> {
+  File? pickedImage;
   bool spwd = true;
   bool loading = false;
   final _formKey = GlobalKey<FormState>();
-  final emailController = TextEditingController();
-  final passwordController = TextEditingController();
+  final _fnamecon = TextEditingController();
+  final _lnamecon = TextEditingController();
+  final _emailcon = TextEditingController();
+  final _monocon = TextEditingController();
+  final _passwordcon = TextEditingController();
   FirebaseAuth _auth = FirebaseAuth.instance;
 
-  void signup() {
-    setState(() {
-      loading = true;
-    });
-    _auth
-        .createUserWithEmailAndPassword(
-            email: emailController.text.toString(),
-            password: passwordController.text.toString())
-        .then((value) {
-      Navigator.push(
-          context, MaterialPageRoute(builder: (context) => GoogleLocation()));
-      setState(() {
-        loading = false;
-      });
-    }).onError((error, stackTrace) {
-      Utils().tostmessage(error.toString());
-      setState(() {
-        loading = false;
-      });
-    });
-  }
+  // void signup() {
+  //   setState(() {
+  //     loading = true;
+  //   });
+  //   _auth
+  //       .createUserWithEmailAndPassword(
+  //           email: _emailcon.text.toString(),
+  //           password: _passwordcon.text.toString())
+  //       .then((value) {
+  //     Navigator.push(
+  //         context, MaterialPageRoute(builder: (context) => GoogleLocation()));
+  //     setState(() {
+  //       loading = false;
+  //     });
+  //   }).onError((error, stackTrace) {
+  //     Utils().tostmessage(error.toString());
+  //     setState(() {
+  //       loading = false;
+  //     });
+  //   });
+  // }
 
   @override
   void dispose() {
     // TODO: implement dispose
     super.dispose();
-    emailController.dispose();
-    passwordController.dispose();
+    _emailcon.dispose();
+    _passwordcon.dispose();
+  }
+
+  showAlertBox() {
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Pick Image From"),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ListTile(
+                onTap: () {
+                  pickImage(ImageSource.camera);
+                  Get.back();
+                },
+                leading: Icon(Icons.camera_alt),
+                title: Text("Camera"),
+              ),
+              ListTile(
+                onTap: () {
+                  pickImage(ImageSource.gallery);
+                  Get.back();
+                },
+                leading: Icon(Icons.image),
+                title: Text("Gallery"),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  addcategory(String cName) async {
+    if (cName == null && pickedImage == null) {
+      return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text("Enter Required Field"),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    Get.back();
+                  },
+                  child: Text("Ok")),
+            ],
+          );
+        },
+      );
+    } else {
+      uploadData();
+    }
+  }
+
+  uploadData() async {
+    UploadTask uploadtask = FirebaseStorage.instance
+        .ref("userimg")
+        .child("${_monocon.text}")
+        .putFile(pickedImage!, SettableMetadata(contentType: 'image/jpeg'));
+    TaskSnapshot taskSnapshot = await uploadtask;
+    String url = await taskSnapshot.ref.getDownloadURL();
+    FirebaseFirestore.instance.collection("userDetails").doc("12").set({
+      "fname": _fnamecon.text.toString(),
+      "uimage": url,
+      "lname": _lnamecon.text.toString(),
+      "emailid": _emailcon.text.toString(),
+      "mono": _monocon.text.toString(),
+      "password": _passwordcon.text.toString()
+    }).then((value) {
+      log("User Uploaded");
+    });
+    // await _collectionReference
+    //     .doc(_cname.toString())
+    //     .set({"cname": _cname.text.toString(), "cimage": url}).then(
+    //   (value) {
+    //     print("data Added");
+    //   },
+    // );
   }
 
   @override
@@ -67,10 +157,54 @@ class _SignUpState extends State<SignUp> {
                 SizedBox(
                   height: dimension.height65,
                 ),
-                Container(
-                  height: dimension.height90,
-                  decoration: BoxDecoration(
-                      shape: BoxShape.circle, color: AppColors.Colorq),
+                GestureDetector(
+                  onTap: () {
+                    showAlertBox();
+                  },
+                  child: Stack(
+                    children: [
+                      pickedImage != null
+                          ? Container(
+                              height: dimension.height60 * 2,
+                              width: dimension.height60 * 2,
+                              decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      image: FileImage(pickedImage!),
+                                      fit: BoxFit.cover),
+                                  shape: BoxShape.circle,
+                                  color: AppColors.Colorq.withOpacity(0.05)),
+                            )
+                          : Container(
+                              height: dimension.height60 * 2,
+                              width: dimension.height60 * 2,
+                              decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      image:
+                                          AssetImage("images/addCategoryy.png"),
+                                      fit: BoxFit.cover),
+                                  shape: BoxShape.circle,
+                                  color: AppColors.Colorq.withOpacity(0.05)),
+                            ),
+                      Container(
+                        margin: EdgeInsets.only(
+                            top: dimension.height65, left: dimension.height80),
+                        height: dimension.height50,
+                        width: dimension.height50,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          //   color: AppColors.Colorq
+                        ),
+                        child: IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            size: dimension.icon30,
+                            Icons.camera_enhance,
+                            color: AppColors.Colorq,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
                 SizedBox(
                   height: dimension.height25,
@@ -94,6 +228,7 @@ class _SignUpState extends State<SignUp> {
                               color: AppColors.Colorq.withOpacity(0.05),
                               borderRadius: BorderRadius.circular(7)),
                           child: TextFormField(
+                            controller: _fnamecon,
                             keyboardType: TextInputType.text,
                             cursorColor: Colors.black,
                             style: TextStyle(
@@ -122,6 +257,12 @@ class _SignUpState extends State<SignUp> {
                                 borderRadius: BorderRadius.circular(5.0),
                               ),
                             ),
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return 'Enter First Name';
+                              }
+                              return null;
+                            },
                           ),
                         ),
                         SizedBox(
@@ -132,6 +273,7 @@ class _SignUpState extends State<SignUp> {
                               color: AppColors.Colorq.withOpacity(0.05),
                               borderRadius: BorderRadius.circular(7)),
                           child: TextFormField(
+                            controller: _lnamecon,
                             keyboardType: TextInputType.text,
                             cursorColor: Colors.black,
                             style: TextStyle(
@@ -160,6 +302,12 @@ class _SignUpState extends State<SignUp> {
                                 borderRadius: BorderRadius.circular(5.0),
                               ),
                             ),
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return 'Enter Last Name';
+                              }
+                              return null;
+                            },
                           ),
                         ),
                         SizedBox(
@@ -170,7 +318,7 @@ class _SignUpState extends State<SignUp> {
                               color: AppColors.Colorq.withOpacity(0.05),
                               borderRadius: BorderRadius.circular(7)),
                           child: TextFormField(
-                            controller: emailController,
+                            controller: _emailcon,
                             keyboardType: TextInputType.emailAddress,
                             cursorColor: Colors.black,
                             style: TextStyle(
@@ -204,7 +352,7 @@ class _SignUpState extends State<SignUp> {
                                       r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
                                   .hasMatch(value!);
                               if (value.isEmpty) {
-                                return 'Enter Email';
+                                return 'Enter Email Address';
                               } else if (!emailValid) {
                                 return 'Enter Valid Email';
                               }
@@ -220,6 +368,7 @@ class _SignUpState extends State<SignUp> {
                               color: AppColors.Colorq.withOpacity(0.05),
                               borderRadius: BorderRadius.circular(7)),
                           child: TextFormField(
+                            controller: _monocon,
                             keyboardType: TextInputType.text,
                             cursorColor: Colors.black,
                             style: TextStyle(
@@ -248,6 +397,12 @@ class _SignUpState extends State<SignUp> {
                                 borderRadius: BorderRadius.circular(5.0),
                               ),
                             ),
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return 'Enter Mobile Number';
+                              }
+                              return null;
+                            },
                           ),
                         ),
                         SizedBox(
@@ -258,7 +413,7 @@ class _SignUpState extends State<SignUp> {
                               color: AppColors.Colorq.withOpacity(0.05),
                               borderRadius: BorderRadius.circular(7)),
                           child: TextFormField(
-                            controller: passwordController,
+                            controller: _passwordcon,
                             keyboardType: TextInputType.text,
                             cursorColor: Colors.black,
                             obscureText: spwd,
@@ -309,41 +464,6 @@ class _SignUpState extends State<SignUp> {
                         SizedBox(
                           height: dimension.height20,
                         ),
-                        Container(
-                          decoration: BoxDecoration(
-                              color: AppColors.Colorq.withOpacity(0.05),
-                              borderRadius: BorderRadius.circular(7)),
-                          child: TextFormField(
-                            keyboardType: TextInputType.text,
-                            cursorColor: Colors.black,
-                            style: TextStyle(
-                              fontSize: 18,
-                              color: Colors.black,
-                            ),
-                            onChanged: (value) {
-                              setState(() {});
-                            },
-                            decoration: InputDecoration(
-                              floatingLabelBehavior:
-                                  FloatingLabelBehavior.always,
-                              labelText: "Confirm Password",
-                              labelStyle: GoogleFonts.poppins(
-                                  color: AppColors.Colorq,
-                                  fontSize: 17,
-                                  fontWeight: FontWeight.w300),
-                              contentPadding: EdgeInsets.fromLTRB(5, 10, 5, 0),
-                              focusedBorder: OutlineInputBorder(
-                                borderSide: BorderSide(color: AppColors.Colorq),
-                                borderRadius: BorderRadius.circular(5.0),
-                              ),
-                              enabledBorder: OutlineInputBorder(
-                                borderSide:
-                                    BorderSide(color: Colors.transparent),
-                                borderRadius: BorderRadius.circular(5.0),
-                              ),
-                            ),
-                          ),
-                        ),
                       ],
                     )),
                 SizedBox(
@@ -352,8 +472,9 @@ class _SignUpState extends State<SignUp> {
                 Bounce(
                   duration: Duration(milliseconds: 200),
                   onPressed: () {
-                    if(_formKey.currentState!.validate()){
-                      signup();
+                    if (_formKey.currentState!.validate()) {
+                      // signup();
+                      addcategory(_fnamecon.text.toString());
                     }
                   },
                   child: Container(
@@ -378,5 +499,21 @@ class _SignUpState extends State<SignUp> {
         ),
       ),
     );
+  }
+
+  pickImage(ImageSource imageSource) async {
+    try {
+      final photo = await ImagePicker().pickImage(source: imageSource);
+      if (photo == null) {
+        return;
+      }
+      final tempImage = File(photo.path);
+      setState(() {
+        pickedImage = tempImage;
+      });
+    } catch (ex) {
+      log(ex.toString());
+      print(ex.toString());
+    }
   }
 }
